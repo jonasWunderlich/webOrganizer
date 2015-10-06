ae74f367c929516d97ec92aad05300ec7fcb2de2
Jonas Wunderlich <jonas.wunderlich@zalari.de>
2015-10-06 16:16:36 +0200

    some functions added to background.js for testing

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -8,3 +8,22 @@ chrome.browserAction.setBadgeText({ text: '\'Allo' });
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
 //# sourceMappingURL=background.js.map
+
+
+chrome.contextMenus.create({
+    title: "This is your chrome extension menu!",
+    id:'selectFromContextMenu',
+    contexts: ["all"]
+  }, function() {
+      var error = chrome.runtime.lastError;
+      if(error) {
+        console.log(error);
+      }
+      else {
+        console.log('Context menu created');
+      }
+  });
+
+chrome.contextMenus.onClicked.addListener(function(tab) {
+      alert('Hello world');
+});
