e1909fcc7283435527ecf0bfd2a32d135436a344
Jonas Wunderlich <jonas.wunderlich@zalari.de>
2015-10-23 06:06:31 +0200

    visualise & activate tab // Bookmark create / delete

--- app/scripts/app/controllers/controllers.js
+++ app/scripts/app/controllers/controllers.js
@@ -18,7 +18,7 @@ angular.module('newTab')
      */
     var getData = function() {
       BrowserHistory.getProcessedHistory().then(function(d) {
-        $scope.allSites = d;
+        $scope.allSites = d.reverse();
       });
       BrowserHistory.getBookmarks().then(function(d) {
         $scope.bookmarks = d;
@@ -41,11 +41,46 @@ angular.module('newTab')
       });
     };
 
+    /**
+     * @ngdoc method
+     * @name _init
+     * @methodOf newTab.MainController
+     * @description Run Functions at Startup
+     */
+    var _init = function() {
+      getData();
+    };
+
+    _init();
+
+
+    $scope.getContextColor = function(site) {
+      if(site.context) {
+        if(site.context.indexOf('neutral') !== 0) {
+          return 'background:'+$scope.storedContexts[site.context]['color'];
+        }
+      }
+    };
+
+    $scope.getContextColorIfBookmark = function(site) {
+      if(site.context && site.bookmark) {
+        if(site.context.indexOf('neutral') !== 0) {
+          return 'background:'+$scope.storedContexts[site.context]['color'];
+        }
+      }
+    };
+
+    $scope.isTab = function(site) {
+      return site.tab;
+    };
+
     $scope.getPanelClasses = function(site) {
-      var _visited, _bookmark, _tab;
+      var _visited, _bookmark, _tab, _context;
 
       _bookmark = (site.bookmark) ? 'bookmark' : '';
-      _tab = (site.tabOpen) ? 'tab' : '';
+      _tab = (site.tab) ? 'tab' : '';
+
+      _context = '';
 
       switch (site.visitCount > 1) {
         case (site.visitCount < 5):
@@ -58,24 +93,48 @@ angular.module('newTab')
           _visited = 'visits-3';
           break;
       }
-      return _visited + ' ' + _bookmark + ' ' + _tab;
+      return _visited + ' ' + _bookmark + ' ' + _tab + ' ' + _context;
     };
 
-    $scope.getContextColor = function(site) {
-      if(site.context)
-        return 'background:'+$scope.storedContexts[site.context]['color']+'';
+    $scope.setBookmark = function(site) {
+      if(site.bookmark) {
+        chrome.bookmarks.remove(site.bookmark, function(result) {
+          $log.debug("Bookmark removed:", result);
+          site.bookmark = false;
+        });
+      } else {
+        chrome.bookmarks.create({'parentId': site.context,
+          'title': site.title,
+          'url': site.url
+        }, function(result) {
+          $log.debug("Bookmark added:", result);
+          site.bookmark = result.id;
+        });
+      }
     };
 
-    /**
-     * @ngdoc method
-     * @name _init
-     * @methodOf newTab.MainController
-     * @description Run Functions at Startup
-     */
-    var _init = function() {
-      getData();
+    $scope.activateTab = function(tab) {
+      $log.debug('trying to activate tab', tab);
+      //chrome.browserAction.enable(tab.id);
+      chrome.tabs.update(tab.id, {selected: true});
     };
 
-    _init();
+    $scope.createTabs = function(contextId) {
+      $scope.storageData.deaktivatedTabs[contextId].forEach(function(tab) {
+        chrome.tabs.create(tab, function(callback) {
+
+          //options = {
+          //  windowId: 0,
+          //  index: 0,
+          //  url: 0,
+          //  active: 0,
+          //  selected: 0,
+          //  pinned: 0,
+          //  openerTabId: 0
+          //};
+
+        });
+      });
+    };
 
   });
